09cccbd1dc781ab630d3c0c9d00e48e82d175769
evelyn henriquez <evemartinahm@gmail.com>
2021-06-23 18:17:01 -0400

    integracion yars

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 
 const { crearArchivo} = require('./helpers/multiplicar')
+const argv = require('yargs').argv;
+
 console.clear()
-// const base = 3;
-console.log(process.argv)
 
-const[,,arg3 ='base=5'] = process.argv;
-const [,base] = arg3.split('=');
+console.log(argv)
 
-console.log(arg3 )
-crearArchivo(base)
-    .then(nombreArchivo=>console.log(nombreArchivo,'creado'))
-    .catch(error=>console.log(error));
+   
+
+// console.log(arg3 )
+// crearArchivo(base)
+//     .then(nombreArchivo=>console.log(nombreArchivo,'creado'))
+//     .catch(error=>console.log(error));
